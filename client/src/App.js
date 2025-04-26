25b8186961253fc5d3b27860a4a9d80e4b8d1894
refrigerator2 <ti.mak4710@gmail.com>
2025-04-26 14:55:43 +0200

    д

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from 'routes/app.routes'
+import {FirstPage} from "./components/firstPage/firstPage";
 import './App.scss'
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './components/login/login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Routes>
+        <Route path="/" element={<FirstPage />} />
+        <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 

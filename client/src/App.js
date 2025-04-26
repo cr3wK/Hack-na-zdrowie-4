c57233da4479357b552a5766afe0f3439986a3e2
refridgerator2 <102374780+refrigerator2@users.noreply.github.com>
2025-04-26 14:57:46 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from 'routes/app.routes'
+import {FirstPage} from "./components/firstPage/firstPage";
 import './App.scss'
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './components/login/login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Routes>
+        <Route path="/" element={<FirstPage />} />
+        <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 
 export default App
+
